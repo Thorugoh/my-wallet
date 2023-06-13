b82b87cb8f05694221e1c3a4b63ef7e633420279
Victor Hugo <pavictorhugo@gmail.com>
2023-06-13 20:29:49 -0300

    feat(common-components): Add font sora_600Sembiold

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { Routes } from './src/routes';
-import { useFonts, Sora_400Regular} from "@expo-google-fonts/sora";
+import { useFonts, Sora_400Regular, Sora_600SemiBold} from "@expo-google-fonts/sora";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Sora_400Regular,
+    Sora_600SemiBold,
   })
 
   if(!fontsLoaded){
     return null;
   }
-  
+
   return (
       <>
         <StatusBar style="auto" />
