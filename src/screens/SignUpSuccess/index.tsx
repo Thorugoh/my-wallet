9b73d996e7f3396a9f6d302d7762b78b9eb76025
Victor Hugo <pavictorhugo@gmail.com>
2023-06-14 01:19:33 -0300

    feat(signup): Create success signup screen

---
+++ src/screens/SignUpSuccess/index.tsx
@@ -0,0 +1,53 @@
+import { useEffect } from "react";
+import { View } from "react-native";
+import { useDispatch } from "react-redux";
+import { Button } from "../../components/Button";
+import { Text } from "../../components/Text";
+import { ScreenAuthProps } from "../../routes/app.auth.routes";
+import { login as loginAction } from "../../slices/userSlice";
+
+import { styles } from "./styles";
+import { Steps } from "../../components/Steps";
+
+export type SignUpSuccessScreenProps = {
+  user: {
+    firstName: string;
+    lastName: string;
+    email: string;
+  }
+}
+
+type SignUpSuccessProps = ScreenAuthProps<"SignUpSuccess">;
+
+export function SignUpSuccess({ navigation, route }: SignUpSuccessProps) {
+  const dispatch = useDispatch();
+  const { user } = route.params;
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitleAlign: "center",
+      headerTitle: props => <Steps totalSteps={3} completeSteps={3} />,
+    });
+  }, [navigation])
+
+  function goToHome() {
+      dispatch(loginAction({
+        id: "some-uui",
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+      }));
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Congratulations!</Text>
+      <Text style={styles.title}>Your account has been successfully created. Please proceed to the app.</Text>
+      
+      <Button title="Continue" onPress={goToHome} />
+      
+    </View>
+  )
+}
+
+
---
+++ src/screens/SignUpSuccess/styles.ts
@@ -0,0 +1,58 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../utils/colors";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: colors.WHITE
+  },
+  title: {
+    fontSize: 18,
+    fontFamily: "Sora_600SemiBold",
+    marginTop: 20,
+    marginBottom: 34,
+    alignSelf: "center"
+  },
+  form: {
+    gap: 20,
+  },
+  loginText: {
+    marginTop: 13,
+    alignSelf: "center",
+    fontSize: 12,
+    color: colors.GREY_700
+  },
+  termsContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    gap: 10,
+  },
+  terms: {
+    marginTop: 19,
+    alignSelf: "center",
+    fontSize: 12,
+    color: colors.GREY_700
+  },
+  termsLink: {
+    color: colors.BLACK,
+  },
+  loginLink: {
+    textDecorationLine: "underline",
+    color: colors.BLACK,
+  },
+  stepContainer: {
+    gap: 5, 
+    flexDirection: "row",
+  },
+  stepItem: {
+    backgroundColor: colors.GREY_100,
+    height: 5, 
+    width: 50, 
+    borderRadius: 100
+  },
+  completeStep: {
+    backgroundColor: colors.PURPLE,
+  }
+})
