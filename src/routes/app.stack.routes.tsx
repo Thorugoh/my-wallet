581542d364419bebd49f29b2979d5c891cd5887f
Victor Hugo <pavictorhugo@gmail.com>
2023-06-14 10:09:16 -0300

    feat(home): Add header to home screen

--- src/routes/app.stack.routes.tsx
+++ src/routes/app.stack.routes.tsx
@@ -1,22 +1,24 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack';
 import { AssetDetails } from '../screens/AssetDetails';
 import { Tabs } from './app.tab.routes';
 
 export type RootStackRouteParams = {
-    Tabs: undefined;
-    Login: undefined;
-    SignUp: undefined;
-    AssetDetails: undefined;
-  };
+  Tabs: undefined;
+  Login: undefined;
+  SignUp: undefined;
+  AssetDetails: undefined;
+};
 
-  
+export type ScreenStackProps<RouteName extends keyof RootStackRouteParams> =
+  NativeStackScreenProps<RootStackRouteParams, RouteName>;
+
 const { Navigator, Screen } = createNativeStackNavigator<RootStackRouteParams>();
 
-export function AppStackRoutes(){
-    return(
-        <Navigator initialRouteName="Tabs" screenOptions={{headerShown: false}}>
-            <Screen name="Tabs" component={Tabs} />
-            <Screen name="AssetDetails" component={AssetDetails} />
-        </Navigator>
-    )
+export function AppStackRoutes() {
+  return (
+    <Navigator initialRouteName="Tabs" screenOptions={{ headerShown: false }}>
+      <Screen name="Tabs" component={Tabs} />
+      <Screen name="AssetDetails" component={AssetDetails} />
+    </Navigator>
+  )
 }
--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -1,22 +1,39 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Home } from '../screens/Home';
+import { Header } from '../screens/Home/components/Header';
 import { Portfolio } from '../screens/Portfolio';
 import { Trade } from '../screens/Trade';
 
-export type RootStackRouteParams = {
-    home: undefined;
-    trade: undefined;
-    portfolio: undefined;
+export type RootTabsRouteParams = {
+  Home: undefined;
+  Trade: undefined;
+  Portfolio: undefined;
 }
+
+export type ScreenTabProps<RouteName extends keyof RootTabsRouteParams> =
+  NativeStackScreenProps<RootTabsRouteParams, RouteName>;
 
 const Tab = createBottomTabNavigator();
 
-export function Tabs(){
-    return(
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Trade" component={Trade} />
-            <Tab.Screen name="Portfolio" component={Portfolio} />
-        </Tab.Navigator>
-    )
+export function Tabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen 
+        name="Home" 
+        options={{
+          header: () => <Header />
+        }}
+        component={Home} 
+      />
+      <Tab.Screen 
+        name="Trade" 
+        component={Trade} 
+      />
+      <Tab.Screen 
+        name="Portfolio" 
+        component={Portfolio} 
+      />
+    </Tab.Navigator>
+  )
 }
